fb12fdaed3ba76d5067806f81f2d2c70ab5193ae
hishmatrai <hishmatrai.2018@gamil.com>
2024-06-12 07:38:53 -0700

    update

--- src/about.js
+++ src/about.js
@@ -1,7 +1,28 @@
 import React, { useState } from "react";
+
 const About = () => {
   let [inputValue, setInputValue] = useState("");
   let [todoList, setTodoList] = useState([]);
+  const [messageType, setMessageType] = useState("");
+  const addHandler = () => {
+    if (inputValue === "") {
+      // alert("Type something ....");
+      setMessageType("error");
+      setTimeout(() => {
+        setMessageType("");
+      }, 2000);
+    } else {
+      setMessageType("success");
+      let newTodo = [];
+      newTodo.push(inputValue);
+      console.log(newTodo);
+      setTodoList([...todoList, ...newTodo]);
+      setInputValue("");
+      setTimeout(() => {
+        setMessageType("");
+      }, 2000);
+    }
+  };
   return (
     <div>
       <h1>About Page</h1>
@@ -14,19 +35,45 @@ const About = () => {
       <button
         onClick={() => {
           if (inputValue === "") {
-            alert("Type something ....");
+            // alert("Type something ....");
+            setMessageType("error");
+            setTimeout(() => {
+              setMessageType("");
+            }, 2000);
           } else {
             let newTodo = [];
             newTodo.push(inputValue);
             console.log(newTodo);
             setTodoList([...todoList, ...newTodo]);
             setInputValue("");
+            setMessageType("success");
+            setTimeout(() => {
+              setMessageType("");
+            }, 2000);
           }
         }}
       >
         Add
       </button>
+      <button onClick={addHandler}>Add 2</button>
+      {/* {messageType !== "" &&
+        (messageType === "error" ? (
+          <p style={{ color: "red" }}>Type something ...</p>
+        ) : (
+          <p style={{ color: "green" }}>Success</p>
+        ))} */}
 
+      {messageType !== "" && (
+        <p style={{ color: messageType === "error" ? "red" : "green" }}>
+          {messageType === "error" ? "Type something ..." : "Success"}
+        </p>
+      )}
+
+      {/* {messageType === "error" ? (
+        <p style={{ color: "red" }}>Type something ...</p>
+      ) : (
+        <p style={{ color: "green" }}>Success</p>
+      )} */}
       {todoList.length === 0 ? (
         <p>Data not found!</p>
       ) : (
--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,93 @@
-import React from "react";
-const ContactUs = ()=>{
-    return(
-        <div>
-            <h1>Contact us Page</h1>
-        </div>
-    )
-}
-export default ContactUs
+import React, { useState } from "react";
+import "./contact.css";
+const ContactUs = () => {
+  const [pages, setPages] = useState([
+    {
+      title: "Home",
+      selected: false,
+    },
+    {
+      title: "About",
+      selected: false,
+    },
+    {
+      title: "Contact",
+      selected: false,
+    },
+    {
+      title: "Gallery",
+      selected: false,
+    },
+  ]);
+  const List = [
+    {
+      title: "A",
+      bgColor: "red",
+      borderColor: "blue",
+      borderRadius: "10px",
+    },
+    {
+      title: "B",
+      bgColor: "green",
+      borderColor: "yellow",
+      borderRadius: "5px",
+    },
+    {
+      title: "C",
+      bgColor: "blue",
+      borderColor: "red",
+      borderRadius: "20px",
+    },
+    {
+      title: "D",
+      bgColor: "yellow",
+      borderColor: "green",
+      borderRadius: "1px",
+    },
+  ];
+  return (
+    <div>
+      <h1>Contact us Page</h1>
+      <ul>
+        {List.map((val, index) => {
+          return (
+            <li
+              key={index}
+              style={{
+                backgroundColor: val.bgColor,
+                // border: "5px solid",
+                // borderColor: val.borderColor,
+
+                border: `5px solid ${val.borderColor}`,
+                borderRadius: val.borderRadius,
+              }}
+            >
+              {val.title}
+            </li>
+          );
+        })}
+      </ul>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      {pages.map((val, index) => {
+        return (
+          <button
+            key={index}
+            onClick={()=> {
+                pages[index].selected = !pages[index].selected;
+                setPages([...pages])
+            }}
+            className={
+              val.selected  ? "contact-active-button" : "contact-button"
+            }
+          >
+            {val.title}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+export default ContactUs;
